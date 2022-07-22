5d5f52bcc90d77f3580eaa3bdd914de55068f0ae
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-22 23:05:21 +0530

    Refactored code to inject dependency to literals using value annotation

--- Spring_Value_Required_Annotation/src/main/java/com/example/value/Student.java
+++ Spring_Value_Required_Annotation/src/main/java/com/example/value/Student.java
@@ -1,18 +1,23 @@
 package com.example.value;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
 	private String name;
 	private String intrestedCourse;
 	private String hobby;
 	
+	@Value("chandu")
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	@Value("java")
 	public void setIntrestedCourse(String intrestedCourse) {
 		this.intrestedCourse = intrestedCourse;
 	}
 	
+	@Value("music")
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
