c73dc8e386cc096f42ef342ee80201b0d0df9407
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-22 22:13:13 +0530

    added code to inject object using autowiring and qualifier anotation

---
+++ Spring_AutoWiring_Qualifier/src/test/java/com/example/aw/SpringAutoWiringQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutoWiringQualifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
