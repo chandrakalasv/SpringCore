b0b1371516d3b0116c9aca36196fcf0ddfc87010
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-23 14:53:48 +0530

    Refactored code to use config class instead of xml file

--- Spring_Common_Annotations/src/main/java/com/example/annotations/Client.java
+++ Spring_Common_Annotations/src/main/java/com/example/annotations/Client.java
@@ -1,12 +1,12 @@
 package com.example.annotations;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		System.out.println("beans.xml file loaded");
+		ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 		College collegeBean = context.getBean("collegeBean", College.class);
 		System.out.println("object created by spring using component annotation" + " " + collegeBean);
 		
---
+++ Spring_Common_Annotations/src/main/java/com/example/annotations/CollegeConfig.java
@@ -0,0 +1,10 @@
+package com.example.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.annotations")
+public class CollegeConfig {
+
+}
