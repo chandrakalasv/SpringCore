aed6988d774ef5cae5f7338a259d5a61d05ce20c
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-25 00:01:34 +0530

    added code to call to init method using post construct annotation in spring

---
+++ Bean_Life_Cycle/src/main/java/com/example/beanlifecycle/StudentDao.java
@@ -0,0 +1,68 @@
+package com.example.beanlifecycle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.annotation.PostConstruct;
+
+public class StudentDao {
+	private String driver;
+	private String url;
+	private String userName;
+	private String password;
+	Connection con;
+	
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@PostConstruct
+	public void init() throws ClassNotFoundException, SQLException {
+		Class.forName(driver);
+		con = DriverManager.getConnection(url, userName, password);
+		
+	}
+	public void selectAllRows() throws ClassNotFoundException, SQLException {
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("select * from ESNew.HostelStudentInfo");
+		while(rs.next()) {
+			int student_id = rs.getInt(1);
+			String student_name = rs.getString(2);
+			double hostelFee = rs.getDouble(3);
+			String foodType = rs.getString(4);
+			System.out.println(student_id + " " + student_name + " " + hostelFee + " " + foodType + " ");
+		}
+		
+	}
+	
+	public void deleteStudentRecord(int student_id) throws ClassNotFoundException, SQLException {
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, userName, password);
+		Statement st = con.createStatement();
+		st.executeUpdate("delete from ESNew.HostelStudentInfo where student_id = " + student_id);
+		System.out.println("student id " + student_id + "deleted"); 
+		
+	}
+	
+	public void closeConnection( ) throws SQLException {
+		con.close();
+	}
+
+
+}
---
+++ Bean_Life_Cycle/src/main/java/com/example/beanlifecycle/Test.java
@@ -0,0 +1,16 @@
+package com.example.beanlifecycle;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("beans.xml file loaded");
+		StudentDao student = context.getBean("student", StudentDao.class);
+		student.selectAllRows();
+		
+	}
+}
